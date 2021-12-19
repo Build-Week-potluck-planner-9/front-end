3a6cd246ce2269576536d5bcd3aa8946ccd48c3a
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2021-12-19 10:48:32 -0500

    home component built just to display

--- buildweek-potluck/src/App.js
+++ buildweek-potluck/src/App.js
@@ -1,10 +1,11 @@
+import Home from './Components/Home';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!
-      </h1>
+      <Home />
     </div>
   );
 }
