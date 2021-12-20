2ced74256136f057fffdb3a85febb010874f3225
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-12-20 14:08:40 -0500

    added axioswithauth and logout code for both components

--- buildweek-potluck/src/utils/axiosWithAuth.js
+++ buildweek-potluck/src/utils/axiosWithAuth.js
@@ -1,3 +1,15 @@
 import axios from 'axios';
 
 //add axiosWithAuth function here
+const axiosWithAuth = () => {
+	const token = localStorage.getItem('token');
+	return axios.create({
+		baseURL: 'http://localhost:9000/api',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `${token}`
+		}
+	});
+};
+
+export default axiosWithAuth;
