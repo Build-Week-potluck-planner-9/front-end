f6fa7154ce2700b381953b7bf56a41d100d846cb
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-12-20 12:53:37 -0500

    added logout structure, added util folder and axioswithauth, added private route file

---
+++ buildweek-potluck/src/Components/PrivateRoute.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+//add PrivateRoute function here
---
+++ buildweek-potluck/src/utils/axiosWithAuth.js
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+//add axiosWithAuth function here
