5a241f0ba8599870c1403d1399ac2e52a01432bc
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2021-12-21 12:53:03 -0500

    event-page is displaying all info correctly.

--- buildweek-potluck/src/Components/Event.js
+++ buildweek-potluck/src/Components/Event.js
@@ -1,40 +1,17 @@
 // All imports here
 import React, { useState } from 'react';
-import mockData from '../mockData';
 import '../App.css';
 
 // start Event component
 export default function Event(props) {
-    const { mockData } = props;
-    const haveList = props.mockData.selectedFood;
-    const neededList = props.mockData.unselectedFood;
-    const addItem = () => {
-        {neededList.map(item => {
-            return haveList.push(item)
-        })}
-        {neededList.map(item => {
-            return neededList.pop(item)
-        })}
-    }
-
     return (
         <div className='event'>
-            <div>
             <img src={props.mockData.img}></img>
-            <h2>{props.mockData.name}</h2>
-            </div>
-            <p><strong>DATE: </strong>{props.mockData.date}</p>
-            <p><strong>TIME: </strong>{props.mockData.time}</p>
-            <p><strong>LOCATION: </strong>{props.mockData.location}</p>
-            <div className='being-brought'>
-                <h4>Item's we already have</h4>
-                <span>{haveList}</span>
-            </div>
-            <div className='item-needed'>
-                <h4>Item's we still need</h4>
-                {neededList.map(item => {
-                    return <p key={item.id}>{item} <button onClick={addItem}>Bring!</button></p>
-                })}
+            <div>
+                <h2>{props.mockData.name}</h2>
+                <h4>Time: <p>{props.mockData.time}</p></h4>
+                <h4>Date: <p>{props.mockData.date}</p></h4>
+                <h4>Location: <p>{props.mockData.location}</p></h4>
             </div>
         </div>
     )
--- buildweek-potluck/src/mockData.js
+++ buildweek-potluck/src/mockData.js
@@ -1,7 +1,7 @@
 export default [
     {
-        img: '',
-        id: Math.floor(Math.random() * 1000000),
+        img: 'images/food1.jpg',
+        id: 1,
         name: 'Sandy\'s potluck',
         date: '11/11/2022',
         time: '7:00pm',
@@ -11,8 +11,8 @@ export default [
         confirmed: false
     },
     {
-        img: '',
-        id: Math.floor(Math.random() * 1000000),
+        img: 'images/food2.jpg',
+        id: 2,
         name: 'Mandy\'s potluck',
         date: '01/11/2022',
         time: '5:00pm',
@@ -22,8 +22,8 @@ export default [
         confirmed: false
     },
     {
-        img: '',
-        id: Math.floor(Math.random() * 1000000),
+        img: 'images/food3.jpg',
+        id: 3,
         name: 'Randy\'s potluck',
         date: '05/07/2022',
         time: '5:00pm',
@@ -33,23 +33,12 @@ export default [
         confirmed: false
     },
     {
-        img: '',
-        id: Math.floor(Math.random() * 1000000),
+        img: 'images/food4.jpg',
+        id: 4,
         name: 'Emily\'s potluck',
         date: '07/07/2022',
         time: '3:00pm',
         location: '7619 clambo drive',
-        selectedFood: ['Chips & Dips'],
-        unselectedFood: ['Salamander Strips', 'Turkey that is mad', 'some fish'],
-        confirmed: false
-    },
-    {
-        img: '',
-        id: Math.floor(Math.random() * 1000000),
-        name: 'Klorply\'s potluck',
-        date: '09/21/2022',
-        time: '1:00pm',
-        location: '31619 rondmond drive',
         selectedFood: ['Chips & Dips'],
         unselectedFood: ['Salamander Strips', 'Turkey that is mad', 'some fish'],
         confirmed: false
