1cd9d27eaa69baac2bb84319417b2586559fe5b9
Tuan147 <90153951+Tuan147@users.noreply.github.com>
2021-12-22 11:26:21 -0500

    added in homepage stylings

--- buildweek-potluck/src/Components/Home.js
+++ buildweek-potluck/src/Components/Home.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const HomeStyles = styled.div`
+
+    background-color: #F9F1D9;
+    min-height: 100%;
+    color: #859D3C;
+
+    h1 {
+        font-size: 5em;
+        padding-bottom: 5%;
+        padding-top: 5%;
+        font-weight: bold;
+    }
+
+    .links{
+        text-decoration: underline #ED732E;
+    }
+`
+
 
 export default function Home() {
     return (
+        <HomeStyles>
         <section className='homepage'>
             <div className='homeWrap'>
                 <h1>Welcome to PotluckPlanner!</h1>
@@ -11,5 +32,6 @@ export default function Home() {
                 </div>
             </div>
         </section>
+        </HomeStyles>
     )
 }
