646913a3c6ff60a4c28a26e87a740d3138b7748a
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2021-12-22 16:31:05 -0500

    created editEvent component for user/admin

--- buildweek-potluck/src/Components/Event.js
+++ buildweek-potluck/src/Components/Event.js
@@ -1,39 +1,32 @@
 // All imports here
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../App.css';
 
 // start Event component
 export default function Event(props) {
-    const [foodWeHave, setFoodWeHave] = useState(props.mockData.selectedFood);
-    const [needed] = useState(props.mockData.unselectedFood);
-
-
-    const addItem = () => {
-    }
-
     return (
         <div className='event'>
             <h2>{props.mockData.name}</h2>
-            <img src={props.mockData.img}></img>
+            <img src={props.mockData.img} alt=''></img>
                 <div className='info'>
                     <h4>Time: <p>{props.mockData.time}</p></h4>
                     <h4>Date: <p>{props.mockData.date}</p></h4>
                     <h4>Location: <p>{props.mockData.location}</p></h4>
                 </div>
-                <div className='items'>
-                    <div className='have'>
-                        <h4>Food already here: </h4>
-                        {foodWeHave.map(item => {
-                            return <p>{item}</p>
-                        })}
+                <div className='food'>
+                    <div className='whatWeHave'>
+                        <h4>Here's what we already have:</h4>
+                        {props.mockData.selectedFood.map(item => (<p key={props.mockData.id}>{item}</p>))}
                     </div>
-                    <div className='needed'>
-                        <h4>Food we need: </h4>
-                        {needed.map(item => {
-                            return <button onClick={addItem}>{item}</button>
-                        })}
+                    <div className='whatWeNeed'>
+                        <h4>Here's what we still need:</h4>
+                        {props.mockData.unselectedFood.map(item => (<p key={item.id}>{item}</p>))}
                     </div>
                 </div>
+                <label><h4>Attending?</h4>
+                <input type='checkbox' onClick={() => (!props.mockData.confirmed)}></input>
+                </label>
         </div>
     )
 }
