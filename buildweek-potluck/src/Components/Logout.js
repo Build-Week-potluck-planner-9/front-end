c33ab04d51e70b3c639598cfc53e2614c84e55b6
Andrew Gary <andrew.gary91@gmail.com>
2021-12-22 12:59:32 -0600

    added styling to logout page

--- buildweek-potluck/src/Components/Logout.js
+++ buildweek-potluck/src/Components/Logout.js
@@ -6,22 +6,20 @@ import { Link } from 'react-router-dom'
 
 const LogoutStyle = styled.div`
 
-    // background-color: #F9F1D9;
-    // min-height: 100%;
-    // color: #859D3C;
+    background-color: #F9F1D9;
+    min-height: 100%;
+    color: #859D3C;
 
-    // h1 {
-    //     font-size: 5em;
-    //     padding-bottom: 5%;
-    //     padding-top: 5%;
-    //     font-weight: bold;
-    // }
+    h1 {
+        font-size: 5em;
+        padding-bottom: 5%;
+        padding-top: 5%;
+        font-weight: bold;
+    }
 
     .links{
         text-decoration: underline #ED732E;
     }
-
-	border: black 1px solid;
 `
 
 export default function Logout() {
